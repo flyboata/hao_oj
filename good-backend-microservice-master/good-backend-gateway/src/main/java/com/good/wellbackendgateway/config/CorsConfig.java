85672575d6ca2777f3cc5b6cee0cb6360a98b819
flyboata <93869638+flyboata@users.noreply.github.com>
2024-04-02 22:02:10 +0800

    update

--- good-backend-microservice-master/good-backend-gateway/src/main/java/com/good/wellbackendgateway/config/CorsConfig.java
+++ good-backend-microservice-master/good-backend-gateway/src/main/java/com/good/wellbackendgateway/config/CorsConfig.java
@@ -18,7 +18,6 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
-        // todo 实际改为线上真实域名、本地域名
         config.setAllowedOriginPatterns(Arrays.asList("*"));
         config.addAllowedHeader("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
